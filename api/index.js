f4033e32caa3b6b30dc3abca62dc9829c84244bf
Darrickisar <118645566+Darrickisar@users.noreply.github.com>
2025-03-27 12:23:49 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,8 +9,8 @@ app.use(bodyParser.json());
 
 // 初始化 Blob 实例
 const blob = new Blob({
-  projectId: prj_bfvK4fHtcklMayiHZEhNkEOBRpfx,
-  token: vercel_blob_rw_XUNFJUgwGuo8v8MC_ETEualt4YVHQJOGCiI9N53Qdhkqj2G
+  projectId: "prj_bfvK4fHtcklMayiHZEhNkEOBRpfx",
+  token: "vercel_blob_rw_XUNFJUgwGuo8v8MC_ETEualt4YVHQJOGCiI9N53Qdhkqj2G"
 });
 
 // 获取计数器数据
