284bb467a9d2d303c0341e21f71cfd36f650cb86
Darrickisar <118645566+Darrickisar@users.noreply.github.com>
2025-03-28 11:57:11 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const path = require('path');
 const app = express();
 app.use(cors());
 
+// 设置静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 处理 API 请求
 app.use('/api', require('./api/index'));
 
+// 所有其他路由返回 index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
